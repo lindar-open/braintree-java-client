31b0aaaf38c29f36d5da6f3a551746af91d8c350
Dafinoiu Iulian <cicorongho@yahoo.com>
2019-03-11 16:13:41 +0000

    create PayPalAccount object from generic Payment Method, format attributes

--- src/main/java/com/lindar/braintree/PayPalAccount.java
+++ src/main/java/com/lindar/braintree/PayPalAccount.java
@@ -10,18 +10,22 @@ import java.util.List;
 
 @Data
 public class PayPalAccount implements PaymentMethod {
-    private String email;
-    private String token;
-    private String billingAgreementId;
-    private boolean isDefault;
-    private String imageUrl;
-    private String payerId;
-    private String customerId;
-    private Calendar createdAt;
-    private Calendar updatedAt;
+    private String             email;
+    private String             token;
+    private String             billingAgreementId;
+    private boolean            isDefault;
+    private String             imageUrl;
+    private String             payerId;
+    private String             customerId;
+    private Calendar           createdAt;
+    private Calendar           updatedAt;
     private List<Subscription> subscriptions;
 
     public static PayPalAccount from(com.braintreegateway.PayPalAccount payPalAccount) {
         return ObjectsAcolyte.copy(payPalAccount, new PayPalAccount());
+    }
+
+    public static PayPalAccount from(com.braintreegateway.PaymentMethod paymentMethod) {
+        return ObjectsAcolyte.copy(paymentMethod, new PayPalAccount());
     }
 }
